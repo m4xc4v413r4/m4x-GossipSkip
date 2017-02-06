43cbe72bcccda0306f4f5f1f126bb7fef612cde6
m4xc4v413r4 <m4xc4v413r4@gmail.com>
2017-02-06 01:20:44 +0000

    Changes - Added the ability to NOT skip gossip by pressing the Shift key when clicking the NPC

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,15 @@
 # Change Log
+## v1.0 - 2017-02-05
+### Added
+- Ability to NOT skip gossip by pressing the Shift key when clicking the NPC
+
 ## v0.8.3 - 2017-02-01
 ### Fixed
-- Updated multiple files to fix Packaging (again)
+- Multiple files to fix Packaging (again)
 
 ## v0.8.2 - 2017-01-31
 ### Fixed
-- Updated multiple files to fix Packaging
+- Multiple files to fix Packaging
 
 ## v0.8.1 - 2017-01-31
 ### Changed
--- m4x-GossipSkip.lua
+++ m4x-GossipSkip.lua
@@ -1,12 +1,18 @@
 local frame = CreateFrame("Frame", "m4xGossipSkip", UIParent);
 
 frame:RegisterEvent("GOSSIP_SHOW");
+frame:RegisterEvent("GOSSIP_CLOSED");
 
 frame:SetScript("OnEvent", function(self, event, ...)
-    if (GetNumGossipAvailableQuests() == 0) and (GetNumGossipActiveQuests() == 0) and (GetNumGossipOptions() == 1) and not ForceGossip() then
+    if event == "GOSSIP_SHOW" and IsShiftKeyDown() then
+        frame:UnregisterEvent("GOSSIP_SHOW");
+        return;
+    elseif event == "GOSSIP_SHOW" and (GetNumGossipAvailableQuests() == 0) and (GetNumGossipActiveQuests() == 0) and (GetNumGossipOptions() == 1) and not ForceGossip() then
         local _, gossipType = GetGossipOptions();
         if gossipType == "gossip" then
             SelectGossipOption(1);
         end
+    elseif event == "GOSSIP_CLOSED" then
+        frame:RegisterEvent("GOSSIP_SHOW");
     end
 end);
