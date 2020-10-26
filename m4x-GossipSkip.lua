686ce64ac7832ff601b7f7d31ac0f7b6aa36f223
m4xc4v413r4 <m4xc4v413r4@users.noreply.github.com>
2020-10-26 12:17:05 +0000

    Fix for API changes

--- m4x-GossipSkip.lua
+++ m4x-GossipSkip.lua
@@ -1,18 +1,17 @@
-local frame = CreateFrame("Frame", "m4xGossipSkip", UIParent);
+local frame = CreateFrame("Frame", "m4xGossipSkip", UIParent)
 
-frame:RegisterEvent("GOSSIP_SHOW");
-frame:RegisterEvent("GOSSIP_CLOSED");
+frame:RegisterEvent("GOSSIP_SHOW")
+frame:RegisterEvent("GOSSIP_CLOSED")
 
 frame:SetScript("OnEvent", function(self, event, ...)
     if event == "GOSSIP_SHOW" and IsShiftKeyDown() then
-        frame:UnregisterEvent("GOSSIP_SHOW");
-        return;
-    elseif event == "GOSSIP_SHOW" and (GetNumGossipAvailableQuests() == 0) and (GetNumGossipActiveQuests() == 0) and (GetNumGossipOptions() == 1) and not ForceGossip() then
-        local _, gossipType = GetGossipOptions();
-        if gossipType == "gossip" then
-            SelectGossipOption(1);
+        frame:UnregisterEvent("GOSSIP_SHOW")
+        return
+    elseif event == "GOSSIP_SHOW" and (C_GossipInfo.GetNumAvailableQuests() == 0) and (C_GossipInfo.GetNumActiveQuests() == 0) and (C_GossipInfo.GetNumOptions() == 1) and not C_GossipInfo.ForceGossip() then
+        if C_GossipInfo.GetOptions()[1]["type"] == "gossip" then
+            C_GossipInfo.SelectOption(1)
         end
     elseif event == "GOSSIP_CLOSED" then
-        frame:RegisterEvent("GOSSIP_SHOW");
+        frame:RegisterEvent("GOSSIP_SHOW")
     end
-end);
+end)
